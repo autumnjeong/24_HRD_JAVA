6f5364a1d216c79014cfdfae0446cc909ebed030
autumnjeong <jeongautumn02@gmail.com>
2024-08-06 11:31:23 +0900

    Add files via upload
    2024.08.06 첫번째 예제

---
+++ Hello.java
@@ -0,0 +1,36 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//한줄 주석
+		
+		/*여러줄 주석*/
+		
+		/**
+		 * 작성일 : 2024.08.06
+		 * 작성자 : 정가을
+		 * 내용 : 자바 기본 출력
+		 */
+		
+		/*System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");
+		System.out.println("Hello Java!");*/
+		
+		//System.out.print("Hello Java! \nHello Java! \nHello Java! \nHello Java! \nHello Java! \nHello Java! \nHello Java! \nHello Java! \nHello Java! \nHello Java! \n ");
+		
+		//System.out.print("Hello Java! \nHello Java! \nHello Java! \n");
+		
+		System.out.println("Hello Java!");
+		
+		
+	}
+
+}
