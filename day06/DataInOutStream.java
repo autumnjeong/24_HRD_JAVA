410a1fdd913bb47d20e1d9f6257e0c961d2e228e
autumnjeong <jeongautumn02@gmail.com>
2024-08-13 16:49:30 +0900

    Add files via upload

--- day06/DataInOutStream.java
+++ day06/DataInOutStream.java
@@ -6,32 +6,33 @@ public class DataInOutStream {
 	
 	static DataOutputStream dos = null;
 	static DataInputStream dis = null;
+	
 	public static void main(String[] args) {
 		
 		try {
-			dos = new DataOutputStream(System.out);
+			dos = new DataOutputStream(new FileOutputStream("file.txt"));
 			dos.writeInt(10);
 			dos.writeDouble(155.2);
 			dos.writeBoolean(true);
 			dos.writeChar('가');
 			dos.writeUTF("문자열 입력하기");
-			
+			dos.flush();
+			dis = new DataInputStream(new FileInputStream("file.txt"));
 			System.out.println("정수값: "+dis.readInt());
 			System.out.println("실수값: "+dis.readDouble());
 			System.out.println("논리값: "+dis.readBoolean());
 			System.out.println("문자값: "+dis.readChar());
 			System.out.println("문자열값: "+dis.readUTF());
 			
+			dos.close();
+			dis.close();
+			
 			
 		}catch(Exception e){
 			e.getMessage();
 		}
 		
-		
-		
-		
-		
-		
 	}
 
 }
+
